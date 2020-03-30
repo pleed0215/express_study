6a03f644a63b8695c1c852ce359e7a93d457f06f
이은덕 <pleed0215@ieundeog-ui-iMac.local>
2020-03-30 16:28:43 +0900

    to 2.22 Home controller part two.. finished.

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,11 +1,13 @@
-export const home = (req, res) => res.render ("home", {pageTitle: "Home"}); // render의 첫번째 인자는 템플릿, 두번째 인자는 템플릿에 추가할 정보가 담긴 객체이다.
+import { videosDb } from "../db";
+
+export const home = (req, res) => res.render ("home", {pageTitle: "Home", videosDb:videosDb}); // render의 첫번째 인자는 템플릿, 두번째 인자는 템플릿에 추가할 정보가 담긴 객체이다.
 export const search = (req, res) => {
     const { query: {term: searchingBy} } = req; // totally new way.
     res.render ("search", {pageTitle: "search", searchingBy: searchingBy});
 }
 
 
-export const videos = (req, res) => res.render("videos", {pageTitle: "videos"});
+export const videos = (req, res) => res.render("videos", {pageTitle: "videos", videosDb: videosDb});
 export const upload = (req, res) => res.render("upload", {pageTitle: "videos/upload"});
 export const editVideo = (req, res) => res.render("editVideo", {pageTitle: "videos/:id/editVideo"});
 export const deleteVideo = (req, res) => res.render ("deleteVideo", {pageTitle: "videos/:id/delete-video"});
---
+++ db.js
@@ -0,0 +1,50 @@
+export const videosDb = [
+    {
+        id: 3245123,
+        title: 'Video awesome',
+        description: 'this is something I fucked',
+        views: 30,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            name: "lee",
+            email: "pleed@naver.com",
+            id: 121212
+        }
+    },
+    {
+        id: 3245123,
+        title: 'Video fucking good',
+        description: 'this is something I fucked',
+        views: 30,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            name: "lee",
+            email: "pleed@naver.com",
+            id: 121212
+        }
+    },
+    {
+        id: 3245123,
+        title: 'Video illegal',
+        description: 'this is something I fucked',
+        views: 30,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            name: "lee",
+            email: "pleed@naver.com",
+            id: 121212
+        }
+    },
+    {
+        id: 3245123,
+        title: 'Video akkk',
+        description: 'this is something I fucked',
+        views: 30,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            name: "lee",
+            email: "pleed@naver.com",
+            id: 121212
+        }
+    }
+]
