5886e21d66d5b290af3e9bf2d50e2bdeb9b7d295
이은덕 <pleed0215@ieundeog-ui-iMac.local>
2020-04-02 13:55:37 +0900

    passport job doing..

--- init.js
+++ init.js
@@ -2,9 +2,13 @@ import app from "./app";
 import "./db";
 import dotenv from "dotenv";
 
+import "./models/Comment";
+import "./models/video";
+import "./models/User";
 
 dotenv.config();
-
-const PORT = process.env.PORT||3000;
-console.log (PORT);
-app.listen(PORT, () => console.log(`🖐  Example app listening on port ${PORT}!`));
+const PORT = process.env.PORT || 3000;
+console.log(PORT);
+app.listen(PORT, () =>
+  console.log(`🖐  Example app listening on port ${PORT}!`)
+);
