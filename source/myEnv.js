106372447d8ba73cab760307c071cba4c5a95e9e
이은덕 <pleed0215@ieundeog-ui-MacBookPro.local>
2020-04-15 20:00:16 +0900

    passport error fixed

--- source/myEnv.js
+++ source/myEnv.js
@@ -1,8 +1,7 @@
 import dotenv from "dotenv";
 import path from "path";
 
-console.log(path.join(__dirname, "../", ".env"));
 const result = dotenv.config({
   path: path.join(__dirname, "../", ".env"),
 });
-console.log(result);
+
