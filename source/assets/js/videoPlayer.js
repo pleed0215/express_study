5e94a77e404eae5a4706d2203e494d31be5bba20
이은덕 <pleed0215@ieundeog-ui-iMac.local>
2020-10-14 16:16:10 +0900

    hh

--- source/assets/js/videoPlayer.js
+++ source/assets/js/videoPlayer.js
@@ -131,7 +131,7 @@ function init() {
 
   // video on load.
   videoPlayer.addEventListener("loadedmetadata", async function () {
-    const videoDuration = await getBlobDuration(videoPlayer.src);
+    const videoDuration = videoPlayer.seekable.end(0);//await getBlobDuration(videoPlayer.src);
     videoLength.innerHTML = secondsToHHMMSS(videoDuration);
     rangeVolume.value = 1;
   });
